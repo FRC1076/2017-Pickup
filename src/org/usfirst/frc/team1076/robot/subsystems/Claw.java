d301d5492407363af928d5d6d5b692b6c4111e0c
Caleb Jones <code@calebjones.net>
2016-11-19 14:14:30 -0500

    Add methods to control the claw intake

--- src/org/usfirst/frc/team1076/robot/subsystems/Claw.java
+++ src/org/usfirst/frc/team1076/robot/subsystems/Claw.java
@@ -1,14 +1,34 @@
 package org.usfirst.frc.team1076.robot.subsystems;
 
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Claw extends Subsystem {
+	
+	SpeedController clawMotor;
+	
+	public Claw(SpeedController clawMotor) {
+		this.clawMotor = clawMotor;
+	}
+	
+	public void setMotorSpeed(double speed) {
+		this.clawMotor.set(speed);
+	}
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	public void intakeBall() {
+		setMotorSpeed(1);
+	}
+	
+	public void expellBall() {
+		setMotorSpeed(-1);
+	}
+	
+	public void stop() {
+		setMotorSpeed(0);
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
