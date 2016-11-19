f85943a273aa90cb63962f589f7a2f77e5926501
Unknown <ellazzhang2012@gmail.com>
2016-11-19 14:07:08 -0500

    Add methods to control motor speeds

--- src/org/usfirst/frc/team1076/robot/subsystems/LeftRightMotors.java
+++ src/org/usfirst/frc/team1076/robot/subsystems/LeftRightMotors.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1076.robot.subsystems;
 
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,8 +8,30 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class LeftRightMotors extends Subsystem {
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	SpeedController leftMotor;
+	SpeedController rightMotor;
+	
+	public LeftRightMotors(SpeedController leftMotor, SpeedController rightMotor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;		
+	}
+	
+	public void setLeftSpeed(double speed) {
+		this.leftMotor.set(speed);
+	}
+	
+	public void setRightSpeed(double speed) {
+		this.rightMotor.set(speed);
+	}
+	
+	public void setSpeed(double speed) {
+		setLeftSpeed(speed);
+		setRightSpeed(speed);
+	}
+	
+	public void stop() {
+		setSpeed(0);
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
