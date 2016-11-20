fcf24ac57d21411c0c0079aee118494a8e2b909a
Unknown <ellazzhang2012@gmail.com>
2016-11-19 23:52:03 -0500

    Add documentation to functions

--- src/org/usfirst/frc/team1076/robot/subsystems/LeftRightMotors.java
+++ src/org/usfirst/frc/team1076/robot/subsystems/LeftRightMotors.java
@@ -15,20 +15,31 @@ public class LeftRightMotors extends Subsystem {
 		this.leftMotor = leftMotor;
 		this.rightMotor = rightMotor;		
 	}
-	
+	/**
+	 * sets the speed of the left motor
+	 * @param speed
+	 */
 	public void setLeftSpeed(double speed) {
 		this.leftMotor.set(speed);
 	}
-	
+	/**
+	 * sets the speed of the right motor
+	 * @param speed
+	 */
 	public void setRightSpeed(double speed) {
 		this.rightMotor.set(speed);
 	}
-	
+	/**
+	 * sets the speed of both left and right motors at the same time, so the robot can move forwards or backwards
+	 * @param speed
+	 */
 	public void setSpeed(double speed) {
 		setLeftSpeed(speed);
 		setRightSpeed(speed);
 	}
-	
+	/**
+	 * stops left and right motors
+	 */
 	public void stop() {
 		setSpeed(0);
 	}
