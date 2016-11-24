56634790056c2e8bf8a380b64f30e057688a270b
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-11-24 15:47:18 -0500

    Add JUnit and unit testing folder.
    JUnit is a library which allows unit testing of individual classes.
    An example unit test was placed in the testsrc folder.

---
+++ testsrc/org/usfirst/frc/team1076/test/ExampleTest.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team1076.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class only serves to show that test
+ *
+ */
+public class ExampleTest {
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
