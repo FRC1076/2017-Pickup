acf66a5e4b889fb0b2a57f9bf5d4d0afd826323a
Anishraja Rajesh <anish.rajesh99@gmail.com>
2016-11-30 20:00:12 -0500

    Reformat doc comments in LeftRightMotors
    Indicate that speed should be in the between -1 and 1

--- src/org/usfirst/frc/team1076/robot/subsystems/LeftRightMotors.java
+++ src/org/usfirst/frc/team1076/robot/subsystems/LeftRightMotors.java
@@ -4,41 +4,53 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * This subsystem represents the motors of the robot, the motor at the front and the
+ * motor at the back.
  */
 public class LeftRightMotors extends Subsystem {
     
 	SpeedController leftMotor;
 	SpeedController rightMotor;
 	
+	/**
+	 * Constructs on instance of LeftRightMotors, passing it two motor controllers
+	 * that represent the left and right motors.
+	 * @param leftMotor
+	 * @param rightMotor
+	 */
 	public LeftRightMotors(SpeedController leftMotor, SpeedController rightMotor) {
 		this.leftMotor = leftMotor;
 		this.rightMotor = rightMotor;		
 	}
+
 	/**
-	 * sets the speed of the left motor
-	 * @param speed
+	 * Sets the speed of the left motor.
+	 * @param speed in the range -1 to 1 inclusive.
 	 */
 	public void setLeftSpeed(double speed) {
 		this.leftMotor.set(speed);
 	}
+
 	/**
-	 * sets the speed of the right motor
-	 * @param speed
+	 * Sets the speed of the right motor.
+	 * @param speed in the range -1 to 1 inclusive.
 	 */
 	public void setRightSpeed(double speed) {
 		this.rightMotor.set(speed);
 	}
+	
 	/**
-	 * sets the speed of both left and right motors at the same time, so the robot can move forwards or backwards
-	 * @param speed
+	 * Sets the speed of both left and right motors at the same time, so the robot can move
+	 * forwards or backwards.
+	 * @param speed in the range -1 to 1 inclusive.
 	 */
 	public void setSpeed(double speed) {
 		setLeftSpeed(speed);
 		setRightSpeed(speed);
 	}
+	
 	/**
-	 * stops left and right motors
+	 * Stops left and right motors.
 	 */
 	public void stop() {
 		setSpeed(0);
