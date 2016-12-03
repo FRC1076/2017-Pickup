f98ba8937a5a48f18ca47a90463504304f4a535a
Unknown <sahmed2026@gmail.com>
2016-12-03 13:27:13 -0500

    Create the Door System

---
+++ src/org/usfirst/frc/team1076/robot/subsystems/DoorPneumatic.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team1076.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**This subsystem /controls the door.
+ *
+ */
+public class DoorPneumatic extends Subsystem {
+    
+	Compressor compressor;
+	Solenoid solenoid;
+	public DoorPneumatic(Compressor compressor, Solenoid solenoid){
+		this.compressor = compressor;
+		this.solenoid = solenoid;
+		
+		
+	}
+	public void open() {
+		solenoid.set(true);
+	}
+	
+	public void close(){
+		solenoid.set(false);
+	}
+	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
