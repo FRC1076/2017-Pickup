ef5620e4923bab4617a55e138e661727797be196
Caleb Jones <code@calebjones.net>
2016-12-09 20:10:25 -0500

    Implement teleop rotation

--- src/org/usfirst/frc/team1076/robot/commands/TeleopCommand.java
+++ src/org/usfirst/frc/team1076/robot/commands/TeleopCommand.java
@@ -31,13 +31,48 @@ public class TeleopCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	// Strafing
-    	double x = gamepad.getAxis(GamepadAxis.LeftX);
-    	double y = gamepad.getAxis(GamepadAxis.LeftY);
-    	frontBack.setSpeed(x);
-    	leftRight.setSpeed(y);
+    	final double x = gamepad.getAxis(GamepadAxis.LeftX);
+    	final double y = gamepad.getAxis(GamepadAxis.LeftY);
     	
-    	// Rotation
+    	// We negate this since we want right (which is positive) to be a
+    	// clockwise rotation, and negative numbers are clockwise angles.
+    	final double rot = -gamepad.getAxis(GamepadAxis.RightX);
+    	
+    	// The robot wheel centers are 14.5" apart for the front and back,
+    	// and 23" apart for the left and right.
+    	// We'll use unit speed for the rotation on the left and right motors,
+    	// and a scaled down speed for the front and back motors, so that they
+    	// move at a speed proportional to their radius.
+    	final double radiusScale = 14.5 / 23;
+
+    	// If rot is a positive angle, we want the following modification:
+    	//   <
+    	// v   ^
+    	//   >
+    	// Which means that frontSpeed should be decreased, back increased,
+    	// left decreased, and right increased.
+    	final double front = x - rot * radiusScale;
+    	final double back = x + rot * radiusScale;
+    	final double left = y - rot;
+    	final double right = y + rot;
+    	
+    	// We don't want any motor to run faster than unit speed, so if anything
+    	// is larger than 1 we'll scale then down.
+    	final double norm = selectMaxAbs(new double[] {1.0, front, back, left, right});
+    	
+    	frontBack.setFrontSpeed(front / norm);
+    	frontBack.setBackSpeed(back / norm);
+    	leftRight.setLeftSpeed(left / norm);
+    	leftRight.setRightSpeed(right / norm);
+    }
+    
+    private double selectMaxAbs(double[] items) {
+    	assert items.length > 0;
+    	double result = Math.abs(items[0]);
+    	for (double item : items) {
+    		result = Math.max(result, Math.abs(item));
+    	}
+    	return result;
     }
 
     // Make this return true when this Command no longer needs to run execute()
