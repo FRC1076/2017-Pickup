ec1cc78ea3b59fe44aeafc53ac69f59b7a2b06ad
Unknown <edx2001@gmail.com>
2017-01-21 19:16:56 -0500

    Start work on AutonomousCommand

--- src/org/usfirst/frc/team1076/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team1076/robot/commands/AutonomousCommand.java
@@ -1,22 +1,24 @@
 package org.usfirst.frc.team1076.robot.commands;
 
-import org.strongback.command.Command;
+import org.strongback.command.CommandGroup;
 import org.strongback.components.Motor;
 
-public class AutonomousCommand extends Command {
+public class AutonomousCommand extends CommandGroup {
 	Motor leftmotor;
 	Motor rightmotor;
 
 	public AutonomousCommand(Motor leftmotor, Motor rightmotor) {
 		this.leftmotor = leftmotor;
 		this.rightmotor = rightmotor;
-	}
-	
-	@Override
-	public boolean execute() {
-		leftmotor.setSpeed(1);
+		
+		// drive 8ft
+		sequentialy(
+		       new ForwardCommand(rightmotor, leftmotor),
+			   new RotateCommand(leftmotor, rightmotor, 5.0, 1),
+			   new ForwardWithVision(rightmotor, rightmotor),
+		
+	    leftmotor.setSpeed(1);
 		rightmotor.setSpeed(1);
-		return false;
+		
 	}
-
 }
